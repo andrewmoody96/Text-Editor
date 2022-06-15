57a2ea60ef27aabfec6e86f564de400cd1c6d916
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-06-15 12:49:00 -0500

    Added event listeners to get install/uninstall working.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,36 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+
+// copied from Week 19 Activity 25
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  butInstall.style.visibility = "visible";
+
+  butInstall.addEventListener("click", () => {
+    event.prompt();
+    butInstall.setAttribute("disabled", true);
+  });
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  
+    const promptEvent = window.deferredPrompt;
+  
+    if (!promptEvent) {
+     return;
+    }
+  
+    promptEvent.prompt();
+    
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log('👍', 'appinstalled', event);
+});

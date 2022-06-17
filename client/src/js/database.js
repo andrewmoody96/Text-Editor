a65da6e215e73c62328a54a49a1ae069a79f04d0
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-06-17 12:24:54 -0500

    database.js finished

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({id: 1, value: content});
+
+  const result = await request;
+
+  console.log('result.value', result.value);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 // Week 19, activity 23
@@ -22,7 +34,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
